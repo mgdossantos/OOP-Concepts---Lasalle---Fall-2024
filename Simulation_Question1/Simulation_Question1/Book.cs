2ac550da35008f1fe79fb34c9c3da67f4f5b766a
Marcela Gonçalves dos Santos <marcela.santos1@uqac.ca>
2024-10-15 19:32:58 -0400

    update Simulation1 Answers

---
+++ Simulation_Question1/Simulation_Question1/Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+//Classes and Objects (20 points):
+//Create a C# class called Book that has private fields for title,
+//author, and price.
+//Add public properties to access these fields.
+//Write a method to display the details of the book. 
+
+public class Book
+{
+	private string title;
+	private string author;
+	private double price;
+
+	// Constructor to initialize a book
+    public Book(string title, string author, double price)
+    {
+		this.title = title;
+		this.author = author;
+		this.price = price;
+    }
+
+    public string Title {  get { return title; } set {  title = value; } }
+	public string Author { get { return author; } set { author = value; } }
+	public double Price { get { return price; } set { price = value; } }
+
+	public string display()
+	{
+		return $"Title: {Title}, Author: {Author}, Price:{Price}";
+	}
+	
+}
---
+++ Simulation_Question1/Simulation_Question1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //Instantiate this class and demonstrate its usage.
+
+        Book book1= new Book();
+        Console.WriteLine(book1.display());
+    }
+}
+
+  
